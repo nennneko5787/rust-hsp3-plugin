ef0e06241e5e43ceda0d1c4ab16f48d426db6594
nennneko5787 <95109758+nennneko5787@users.noreply.github.com>
2026-04-19 11:41:02 +0900

    unsafe 2

--- src/lib.rs
+++ src/lib.rs
@@ -33,87 +33,93 @@ fn next_random() -> u32 {
 }
 
 unsafe fn newcmd2() {
-    let mut pval: *mut PVal = ptr::null_mut();
-    let aptr = (*exinfo).HspFunc_prm_getva.unwrap()(&mut pval);
-    P1 = (*exinfo).HspFunc_prm_getdi.unwrap()(100);
-    if P1 == 0 {
-        (*exinfo).HspFunc_puterror.unwrap()(HSPERROR_HSPERR_DIVIDED_BY_ZERO);
-        return;
+    unsafe {
+        let mut pval: *mut PVal = ptr::null_mut();
+        let aptr = (*exinfo).HspFunc_prm_getva.unwrap()(&mut pval);
+        P1 = (*exinfo).HspFunc_prm_getdi.unwrap()(100);
+        if P1 == 0 {
+            (*exinfo).HspFunc_puterror.unwrap()(HSPERROR_HSPERR_DIVIDED_BY_ZERO);
+            return;
+        }
+        P2 = (next_random() % (P1 as u32)) as i32;
+        (*exinfo).HspFunc_prm_setva.unwrap()(
+            pval,
+            aptr,
+            HSPVAR_FLAG_INT,
+            &raw const P2 as *const _ as *const _,
+        );
     }
-    P2 = (next_random() % (P1 as u32)) as i32;
-    (*exinfo).HspFunc_prm_setva.unwrap()(
-        pval,
-        aptr,
-        HSPVAR_FLAG_INT,
-        &raw const P2 as *const _ as *const _,
-    );
 }
 
 unsafe fn newcmd3() {
-    let p = (*exinfo).HspFunc_prm_gets.unwrap()();
-    if p.is_null() {
-        return;
-    }
-
-    let pathname = match CStr::from_ptr(p).to_str() {
-        Ok(s) => s.to_owned(),
-        Err(_) => {
-            (*exinfo).HspFunc_puterror.unwrap()(HSPERROR_HSPERR_TYPE_MISMATCH);
+    unsafe {
+        let p = (*exinfo).HspFunc_prm_gets.unwrap()();
+        if p.is_null() {
             return;
         }
-    };
 
-    let chk = code_getprm();
-    if chk <= PARAM_END {
-        return;
-    }
+        let pathname = match CStr::from_ptr(p).to_str() {
+            Ok(s) => s.to_owned(),
+            Err(_) => {
+                (*exinfo).HspFunc_puterror.unwrap()(HSPERROR_HSPERR_TYPE_MISMATCH);
+                return;
+            }
+        };
 
-    let out = match (*mpval).flag as i32 {
-        HSPVAR_FLAG_STR => {
-            let s = CStr::from_ptr((*mpval).pt).to_string_lossy();
-            format!("{pathname}\n{s}\n")
-        }
-        HSPVAR_FLAG_DOUBLE => {
-            let ptr = (*mpval).pt as *const f64;
-            format!("{pathname}\n{}\n", *ptr)
-        }
-        HSPVAR_FLAG_INT => {
-            let ptr = (*mpval).pt as *const i32;
-            format!("{pathname}\n{}\n", *ptr)
-        }
-        _ => {
-            (*exinfo).HspFunc_puterror.unwrap()(HSPERROR_HSPERR_TYPE_MISMATCH);
+        let chk = code_getprm();
+        if chk <= PARAM_END {
             return;
         }
-    };
 
-    let mut file = match OpenOptions::new().create(true).append(true).open(&pathname) {
-        Ok(f) => f,
-        Err(_) => {
+        let out = match (*mpval).flag as i32 {
+            HSPVAR_FLAG_STR => {
+                let s = CStr::from_ptr((*mpval).pt).to_string_lossy();
+                format!("{pathname}\n{s}\n")
+            }
+            HSPVAR_FLAG_DOUBLE => {
+                let ptr = (*mpval).pt as *const f64;
+                format!("{pathname}\n{}\n", *ptr)
+            }
+            HSPVAR_FLAG_INT => {
+                let ptr = (*mpval).pt as *const i32;
+                format!("{pathname}\n{}\n", *ptr)
+            }
+            _ => {
+                (*exinfo).HspFunc_puterror.unwrap()(HSPERROR_HSPERR_TYPE_MISMATCH);
+                return;
+            }
+        };
+
+        let mut file = match OpenOptions::new().create(true).append(true).open(&pathname) {
+            Ok(f) => f,
+            Err(_) => {
+                (*exinfo).HspFunc_puterror.unwrap()(HSPERROR_HSPERR_FILE_IO);
+                return;
+            }
+        };
+
+        if file.write_all(out.as_bytes()).is_err() {
             (*exinfo).HspFunc_puterror.unwrap()(HSPERROR_HSPERR_FILE_IO);
-            return;
         }
-    };
-
-    if file.write_all(out.as_bytes()).is_err() {
-        (*exinfo).HspFunc_puterror.unwrap()(HSPERROR_HSPERR_FILE_IO);
     }
 }
 
 unsafe fn newcmd4() {
-    P1 = (*exinfo).HspFunc_prm_getdi.unwrap()(0);
-    P2 = (*exinfo).HspFunc_prm_getdi.unwrap()(0);
-    P3 = (*exinfo).HspFunc_prm_getdi.unwrap()(0);
-    P4 = (*exinfo).HspFunc_prm_getdi.unwrap()(0);
-    P5 = (*exinfo).HspFunc_prm_getdi.unwrap()(0);
+    unsafe {
+        P1 = (*exinfo).HspFunc_prm_getdi.unwrap()(0);
+        P2 = (*exinfo).HspFunc_prm_getdi.unwrap()(0);
+        P3 = (*exinfo).HspFunc_prm_getdi.unwrap()(0);
+        P4 = (*exinfo).HspFunc_prm_getdi.unwrap()(0);
+        P5 = (*exinfo).HspFunc_prm_getdi.unwrap()(0);
 
-    let bm = (*exinfo).HspFunc_getbmscr.unwrap()(*(*exinfo).actscr) as *mut BMSCR;
-    if bm.is_null() {
-        return;
+        let bm = (*exinfo).HspFunc_getbmscr.unwrap()(*(*exinfo).actscr) as *mut BMSCR;
+        if bm.is_null() {
+            return;
+        }
+
+        draw_line(bm, P1, P2, P3, P4, P5);
+        bms_send(bm, 0, 0, (*bm).sx, (*bm).sy);
     }
-
-    draw_line(bm, P1, P2, P3, P4, P5);
-    bms_send(bm, 0, 0, (*bm).sx, (*bm).sy);
 }
 
 #[unsafe(no_mangle)]
@@ -138,34 +144,36 @@ pub extern "C" fn cmdfunc(cmd: i32) -> i32 {
 
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn reffunc(type_res: *mut i32, cmd: i32) -> *mut ::core::ffi::c_void {
-    if *TYPE_ != TYPE_MARK {
-        (*exinfo).HspFunc_puterror.unwrap()(HSPERROR_HSPERR_INVALID_FUNCPARAM);
-    }
-    if *VAL != '(' as i32 {
-        (*exinfo).HspFunc_puterror.unwrap()(HSPERROR_HSPERR_INVALID_FUNCPARAM);
-    }
-    (*exinfo).HspFunc_prm_next.unwrap()();
-
-    match cmd {
-        0x00 => {
-            P1 = (*exinfo).HspFunc_prm_geti.unwrap()();
-            REF_IVAL = P1 * 2;
+    unsafe {
+        if *TYPE_ != TYPE_MARK {
+            (*exinfo).HspFunc_puterror.unwrap()(HSPERROR_HSPERR_INVALID_FUNCPARAM);
         }
-        _ => {
-            (*exinfo).HspFunc_puterror.unwrap()(HSPERROR_HSPERR_UNSUPPORTED_FUNCTION);
+        if *VAL != '(' as i32 {
+            (*exinfo).HspFunc_puterror.unwrap()(HSPERROR_HSPERR_INVALID_FUNCPARAM);
         }
-    }
+        (*exinfo).HspFunc_prm_next.unwrap()();
 
-    if *TYPE_ != TYPE_MARK {
-        (*exinfo).HspFunc_puterror.unwrap()(HSPERROR_HSPERR_INVALID_FUNCPARAM);
-    }
-    if *VAL != ')' as i32 {
-        (*exinfo).HspFunc_puterror.unwrap()(HSPERROR_HSPERR_INVALID_FUNCPARAM);
-    }
-    (*exinfo).HspFunc_prm_next.unwrap()();
+        match cmd {
+            0x00 => {
+                P1 = (*exinfo).HspFunc_prm_geti.unwrap()();
+                REF_IVAL = P1 * 2;
+            }
+            _ => {
+                (*exinfo).HspFunc_puterror.unwrap()(HSPERROR_HSPERR_UNSUPPORTED_FUNCTION);
+            }
+        }
 
-    *type_res = HSPVAR_FLAG_INT;
-    &raw mut REF_IVAL as *mut _ as *mut ::core::ffi::c_void
+        if *TYPE_ != TYPE_MARK {
+            (*exinfo).HspFunc_puterror.unwrap()(HSPERROR_HSPERR_INVALID_FUNCPARAM);
+        }
+        if *VAL != ')' as i32 {
+            (*exinfo).HspFunc_puterror.unwrap()(HSPERROR_HSPERR_INVALID_FUNCPARAM);
+        }
+        (*exinfo).HspFunc_prm_next.unwrap()();
+
+        *type_res = HSPVAR_FLAG_INT;
+        &raw mut REF_IVAL as *mut _ as *mut ::core::ffi::c_void
+    }
 }
 
 #[unsafe(no_mangle)]
@@ -198,72 +206,76 @@ pub extern "C" fn DllMain(_: HINSTANCE, _: u32, _: *mut ::core::ffi::c_void) -> 
 
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn hsp3cmdinit(info: *mut HSP3TYPEINFO) {
-    hsp3sdk_init(info);
-    (*info).cmdfunc = Some(cmdfunc);
-    (*info).reffunc = Some(reffunc);
-    (*info).termfunc = Some(termfunc);
+    unsafe {
+        hsp3sdk_init(info);
+        (*info).cmdfunc = Some(cmdfunc);
+        (*info).reffunc = Some(reffunc);
+        (*info).termfunc = Some(termfunc);
+    }
 }
 
 pub unsafe fn draw_line(bm: *mut BMSCR, x1: i32, y1: i32, x2: i32, y2: i32, color: i32) {
-    let lp_dest = (*bm).pBit as *mut u8;
-    let n_dest_width = (*bm).sx;
-    let n_dest_wbyte = (*bm).sx * 3;
-    let n_dest_height = (*bm).sy;
-    let dw_dest = lp_dest.add(((n_dest_height - 1) * n_dest_wbyte) as usize);
+    unsafe {
+        let lp_dest = (*bm).pBit as *mut u8;
+        let n_dest_width = (*bm).sx;
+        let n_dest_wbyte = (*bm).sx * 3;
+        let n_dest_height = (*bm).sy;
+        let dw_dest = lp_dest.add(((n_dest_height - 1) * n_dest_wbyte) as usize);
 
-    let color1 = (color & 0xffff) as u16;
-    let color2 = ((color & 0xff0000) >> 16) as u8;
+        let color1 = (color & 0xffff) as u16;
+        let color2 = ((color & 0xff0000) >> 16) as u8;
 
-    let mut dx = x2 - x1;
-    let addx = if dx < 0 {
-        dx = -dx;
-        -1
-    } else {
-        1
-    };
+        let mut dx = x2 - x1;
+        let addx = if dx < 0 {
+            dx = -dx;
+            -1
+        } else {
+            1
+        };
 
-    let mut dy = y2 - y1;
-    let addy = if dy < 0 {
-        dy = -dy;
-        -1
-    } else {
-        1
-    };
+        let mut dy = y2 - y1;
+        let addy = if dy < 0 {
+            dy = -dy;
+            -1
+        } else {
+            1
+        };
 
-    let mut x = x1;
-    let mut y = y1;
-    let mut cnt = 0;
+        let mut x = x1;
+        let mut y = y1;
+        let mut cnt = 0;
 
-    if dx > dy {
-        for _ in 0..dx {
-            if y >= 0 && y < n_dest_height && x >= 0 && x < n_dest_width {
-                let up = dw_dest
-                    .offset(-(n_dest_wbyte * y) as isize)
-                    .add((x * 3) as usize);
-                *(up as *mut u16) = color1;
-                *up.add(2) = color2;
-            }
-            cnt += dy;
-            x += addx;
-            if cnt >= dx {
-                cnt -= dx;
-                y += addy;
-            }
-        }
-    } else {
-        for _ in 0..dy {
-            if y >= 0 && y < n_dest_height && x >= 0 && x < n_dest_width {
-                let up = dw_dest
-                    .offset(-(n_dest_wbyte * y) as isize)
-                    .add((x * 3) as usize);
-                *(up as *mut u16) = color1;
-                *up.add(2) = color2;
-            }
-            cnt += dx;
-            y += addy;
-            if cnt >= dy {
-                cnt -= dy;
+        if dx > dy {
+            for _ in 0..dx {
+                if y >= 0 && y < n_dest_height && x >= 0 && x < n_dest_width {
+                    let up = dw_dest
+                        .offset(-(n_dest_wbyte * y) as isize)
+                        .add((x * 3) as usize);
+                    *(up as *mut u16) = color1;
+                    *up.add(2) = color2;
+                }
+                cnt += dy;
                 x += addx;
+                if cnt >= dx {
+                    cnt -= dx;
+                    y += addy;
+                }
+            }
+        } else {
+            for _ in 0..dy {
+                if y >= 0 && y < n_dest_height && x >= 0 && x < n_dest_width {
+                    let up = dw_dest
+                        .offset(-(n_dest_wbyte * y) as isize)
+                        .add((x * 3) as usize);
+                    *(up as *mut u16) = color1;
+                    *up.add(2) = color2;
+                }
+                cnt += dx;
+                y += addy;
+                if cnt >= dy {
+                    cnt -= dy;
+                    x += addx;
+                }
             }
         }
     }
